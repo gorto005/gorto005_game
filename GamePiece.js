c2d9a94130949802aece3c9825527255dc6733e3
notrogd <32966014+notrogd@users.noreply.github.com>
2017-11-09 19:39:13 -0700

    Added fireball

--- GamePiece.js
+++ GamePiece.js
@@ -41,6 +41,7 @@ function GamePiece (type, //npc or player
     this.lasty = y;
 
     this.moved = false;
+    this.dead = false;
 
     this.speedX = speedX;
     this.speedY = speedY;   
@@ -52,67 +53,74 @@ function GamePiece (type, //npc or player
 }
 
 GamePiece.prototype = {
-    update(ctx) {
+    update:function(ctx) {
         ctx.clearRect(this.lastx-3, this.lasty-3, this.image.width+6, this.image.height+6);
-
+        if (this.dead) {
+            var img = ctx.createImageData(this.image.width, this.image.height);
+            for (var i = img.data.length; --i >= 0; )
+                img.data[i] = 0;
+            this.image = img;
+        }
         ctx.drawImage(this.image, 
             this.x, 
             this.y);
         if (this.moved) {
             this.playMoveSound();
-        }
+
         this.moved = false;
         this.image = this.defaultimage;
+        } 
     },
-    playMoveSound(){
+    playMoveSound:function(){
         this.sound.play();
     },
-    stopMoveSound(){
+    stopMoveSound:function(){
         this.sound.pause();
     },
-    move (keys) {
+    move:function(keys) {
         this.lastx = this.x;
         this.lasty = this.y;
+        if (!this.dead) {
+            if (this.type == "player") {
+                if (keys && keys[this.moveLeftKey]) {
+                    this.moved = true;
+                    this.moveLeftImage.src = this.moveLeftImages[this.moveLeftImageCount];
+                    this.image = this.moveLeftImage;
+                    this.x -= this.speedX;
 
-        if (this.type == "player") {
-            if (keys && keys[this.moveLeftKey]) {
-                this.moved = true;
-                this.moveLeftImage.src = this.moveLeftImages[this.moveLeftImageCount];
-                this.image = this.moveLeftImage;
-                this.x -= this.speedX;
-
-                if (this.moveLeftImageCount < this.moveLeftImages.length - 1) {
-                    this.moveLeftImageCount++;
-                } else {
-                    this.moveLeftImageCount = 0;
+                    if (this.moveLeftImageCount < this.moveLeftImages.length - 1) {
+                        this.moveLeftImageCount++;
+                    } else {
+                        this.moveLeftImageCount = 0;
+                    }
                 }
-            }
-            if (keys && keys[this.moveRightKey]) {
-                this.moved = true;
-                this.moveRightImage.src = this.moveRightImages[this.moveRightImageCount];
-                this.image = this.moveRightImage;
-                this.x += this.speedX;
+                if (keys && keys[this.moveRightKey]) {
+                    this.moved = true;
+                    this.moveRightImage.src = this.moveRightImages[this.moveRightImageCount];
+                    this.image = this.moveRightImage;
+                    this.x += this.speedX;
 
-                if (this.moveRightImageCount < this.moveRightImages.length - 1) {
-                    this.moveRightImageCount++;
-                } else {
-                    this.moveRightImageCount = 0;
+                    if (this.moveRightImageCount < this.moveRightImages.length - 1) {
+                        this.moveRightImageCount++;
+                    } else {
+                        this.moveRightImageCount = 0;
+                    }
                 }
+                if (keys && keys[this.moveUpKey]) {
+                    this.moved = true;
+                    this.y -= this.speedY; 
+                }
+                if (keys && keys[this.moveDownKey]) {
+                    this.moved = true;
+                    this.y += this.speedY;
+                }
+            } else if (this.type == "npc") {
+                var speed = Math.floor(Math.random() * 10);
+                var vx = Math.floor((Math.random() * 3) - 1) * speed;
+                var vy = Math.floor((Math.random() * 3) - 1) * speed;
+                this.x -= vx;
+                this.y -= vy;
             }
-            if (keys && keys[this.moveUpKey]) {
-                this.moved = true;
-                this.y -= this.speedY; 
-            }
-            if (keys && keys[this.moveDownKey]) {
-                this.moved = true;
-                this.y += this.speedY;
-            }
-        } else if (this.type == "npc") {
-            var speed = Math.floor(Math.random() * 10);
-            var vx = Math.floor((Math.random() * 3) - 1) * speed;
-            var vy = Math.floor((Math.random() * 3) - 1) * speed;
-            this.x -= vx;
-            this.y -= vy;
         }
     }
 }
